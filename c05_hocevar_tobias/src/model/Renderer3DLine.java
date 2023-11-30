695f58119b0f7bb75f55207b45c4d6599d9ef1fb
Tobias Hočevar <tobiashocevar@gmail.com>
2023-11-30 09:45:01 +0100

    prep for task3

---
+++ c05_hocevar_tobias/src/model/Renderer3DLine.java
@@ -0,0 +1,17 @@
+package model;
+
+import rasterize.FilledLineRasterizer;
+import rasterize.Raster;
+import transforms.Mat4;
+
+public class Renderer3DLine {
+
+    public void render(final Raster raster, final Scene scene, final FilledLineRasterizer flr, int color, final Mat4 viewMatrix, final Mat4 projectionMatrix ){
+
+
+    }
+
+    private void renderObject(){
+
+    }
+}
